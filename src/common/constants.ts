42be70e4ad68239fb7f5a6ac67b2e5170cea41e4
JEB <jebarrocal@gmail.com>
2026-06-17 23:12:12 -0300

    chore: bump version to 0.6.0

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -6,7 +6,7 @@
 export const EVENT_ID_PREFIX = 'evt_';
 
 /** Current version of the events-toolkit library. */
-export const LIBRARY_VERSION = '0.3.0';
+export const LIBRARY_VERSION = '0.6.0';
 
 /** Default major version for NATS subject strings (appended as `v1`). */
 export const DEFAULT_SUBJECT_VERSION = '1';
